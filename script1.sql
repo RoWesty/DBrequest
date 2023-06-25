e90b8d48f0484fd0d34d99f6d71c8b1d608bbfe5
RoWesty <132814003+RoWesty@users.noreply.github.com>
2023-06-25 21:57:37 +0300

    Добавление 3 запроса sql

--- script1.sql
+++ script1.sql
@@ -1,4 +1,4 @@
 select r."ФИО", count(f.id_formuls) OVER(partition by f.id_read) as countbook
-from readers r, formul f , operation o 
+from readers r, formul f , operation o
 where r.id_read = f.id_read and f.id_operations = o.id_operations and o."Дата_выдачи" >= '2023-01-01'
 order by countbook desc;
