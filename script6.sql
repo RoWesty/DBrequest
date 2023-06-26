de36d70793225337e8bf484426fab25000f9fe65
RoWesty <132814003+RoWesty@users.noreply.github.com>
2023-06-26 15:14:20 +0300

    Исправление 7 запроса, добавление 8 запроса

--- script6.sql
+++ script6.sql
@@ -1,4 +1,5 @@
-select h."Название", count(b.id_house) over(partition by b.id_house) as population
+select h."Название", count(b.id_house) as population
 from formul f, book b, house h
 where f.id_books = b.id_books and b.id_house = h.id_house
+GROUP BY h."Название"
 order by population desc
